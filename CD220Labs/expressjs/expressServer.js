f1c54d793553132a92ad1222ea7d8dfd5a13b92e
Extroverted-introvert <parth.tripathi17@gmail.com>
2021-07-21 21:25:42 +0000

    [Addition]: Added API endpoint to fetch month

--- CD220Labs/expressjs/expressServer.js
+++ CD220Labs/expressjs/expressServer.js
@@ -3,6 +3,19 @@ const app = new express();
 
 let loginDetails = [];
 
+function get_month(month_input) {
+  
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];  
+    month_text = "Invalid Month";
+    for (let i = 0; i < months.length; i++) {
+        if (month_input-1==i){
+            month_text = months[i];
+        }  
+    }
+    // console.log(month_input, month_text)
+    return month_text   // The function returns the product of p1 and p2
+}
+
 app.get("/",(req,res)=>{
     res.send("Welcome to the express server")
 })
@@ -19,6 +32,11 @@ app.post("/login/:name",(req,res)=>{
 app.get("/:name",(req,res)=>{
     res.send("Hello "+req.params.name)
 })
+
+app.get("/fetch_month/:num",(req,res)=>{
+    res.send("The month fetched is, " + get_month(req.params.num))
+})
+
 
 app.listen(3333, () => {
     console.log(`Listening at http://localhost:3333`)
